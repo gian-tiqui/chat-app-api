7b7e7f7ccf5f09e9afb1c891144ba845a094b893
gian <michaelgiantiqui3@gmail.com>
2024-05-20 14:24:49 +0800

    fix access later

--- src/main/java/com/gi/Chat/Application/Api/config/WebSocketConfig.java
+++ src/main/java/com/gi/Chat/Application/Api/config/WebSocketConfig.java
@@ -6,14 +6,6 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-/*
-
-  @TODO
-
-  Change this config to make private messaging feats
-
-*/
-
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
@@ -27,6 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/ws")
+        .setAllowedOrigins("*")
         .withSockJS();
   }
 }
